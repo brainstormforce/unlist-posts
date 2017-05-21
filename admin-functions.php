dc2c7ef1fcbc77a9697d4c6bc006d8bd193792d1
Nikhil Chavan <email@nikhilchavan.com>
2017-05-21 20:43:54 +0530

    initial commit

---
+++ admin-functions.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Admin functions for the plugin.
+ *
+ * @package  hide-post
+ */
+
+defined( 'ABSPATH' ) or exit;
+
+/**
+ * Hide_Post_Admin setup
+ *
+ * @since 1.0
+ */
+class Hide_Post_Admin {
+
+	/**
+	 * Instance of Hide_Post_Admin
+	 *
+	 * @var Hide_Post_Admin
+	 */
+	private static $_instance = null;
+
+	/**
+	 * Instance of Hide_Post_Admin
+	 *
+	 * @return Hide_Post_Admin Instance of Hide_Post_Admin
+	 */
+	public static function instance() {
+		if ( ! isset( self::$_instance ) ) {
+			self::$_instance = new self;
+		}
+
+		return self::$_instance;
+	}
+
+	/**
+	 * Constructor
+	 */
+	private function __construct() {
+		add_action( 'add_meta_boxes', array( $this, 'register_metabox' ) );
+		add_action( 'save_post', array( $this, 'save_meta' ) );
+	}
+
+	/**
+	 * Register meta box(es).
+	 */
+	function register_metabox() {
+		$args = array(
+			'public' => true
+		);
+
+		$post_types = get_post_types( $args, 'names', 'and' );
+
+		add_meta_box( 'ehf-meta-box', __( 'Hide Post', 'hide-post' ), array(
+			$this,
+			'metabox_render',
+		), $post_types, 'side', 'high' );
+	}
+
+	/**
+	 * Render Meta field.
+	 *
+	 * @param  POST $post Currennt post object which is being displayed.
+	 */
+	function metabox_render( $post ) {
+		$hidden_posts = get_option( 'hf_hide_post', array() );
+
+		if ( in_array( $post->ID, $hidden_posts ) ) {
+			$checked = 'checked';
+		} else {
+			$checked = '';
+		}
+
+		// We'll use this nonce field later on when saving.
+		wp_nonce_field( 'hide_post_nounce', 'hide_post_nounce' );
+		?>
+		<p>
+			<label class="checkbox-inline">
+				<input name="hf_hide_post" type="checkbox" <?php echo esc_attr( $checked ); ?> value=""><?php _e( 'Hide this post?', 'hide-post' ); ?>
+			</label>
+		</p>
+		<p class="description"><?php _e( 'This will hide the post from your site, The post can only be accessed from direct URL.', 'hide-post' ); ?> </p>
+		<?php
+	}
+
+	/**
+	 * Save meta field.
+	 *
+	 * @param  POST $post_id Currennt post object which is being displayed.
+	 *
+	 * @return Void
+	 */
+	public function save_meta( $post_id ) {
+		// Bail if we're doing an auto save.
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return;
+		}
+
+		// if our nonce isn't there, or we can't verify it, bail.
+		if ( ! isset( $_POST['hide_post_nounce'] ) || ! wp_verify_nonce( $_POST['hide_post_nounce'], 'hide_post_nounce' ) ) {
+			return;
+		}
+
+		// if our current user can't edit this post, bail.
+		if ( ! current_user_can( 'edit_posts' ) ) {
+			return;
+		}
+
+		$hidden_posts = get_option( 'hf_hide_post', array() );
+
+		if ( isset( $_POST['hf_hide_post'] ) ) {
+			array_push( $hidden_posts, $post_id );
+		} elseif ( in_array( $post_id, $hidden_posts ) ) {
+			$key = array_search( $post_id, $hidden_posts );
+			unset( $hidden_posts[ $key ] );
+		}
+
+		update_option( 'hf_hide_post', $hidden_posts );
+	}
+
+}
+
+Hide_Post_Admin::instance();
---
+++ hide-post.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Plugin Name:     Hide Post
+ * Plugin URI:      https://www.brainstormforce.com
+ * Description:     Hide post from dispying anywhere on the site, only access the post with a direct link to the post.
+ * Author:          Brainstorm Force
+ * Author URI:      https://www.brainstormforce.com
+ * Text Domain:     hide-post
+ * Domain Path:     /languages
+ * Version:         1.0.0
+ *
+ * @package         Hide_Post
+ */
+
+defined( 'ABSPATH' ) or exit;
+
+define( 'ODA_DIR', plugin_dir_path( __FILE__ ) );
+define( 'ODA_URI', plugins_url( '/', __FILE__ ) );
+define( 'ODA_VER', '0.1.0' );
+
+
+/**
+ * Hide_Posts setup
+ *
+ * @since 1.0.0
+ */
+if ( ! class_exists( 'Hide_Posts' ) ) {
+	class Hide_Posts {
+
+		/**
+		 * Instance of Hide_Posts
+		 *
+		 * @since  1.0.0
+		 * @var Hide_Posts
+		 */
+		private static $_instance = null;
+
+		/**
+		 * Instance of Hide_Posts
+		 *
+		 * @since  1.0.0
+		 * @return Hide_Posts Instance of Hide_Posts
+		 */
+		public static function instance() {
+			if ( ! isset( self::$_instance ) ) {
+				self::$_instance = new self;
+			}
+
+			return self::$_instance;
+		}
+
+		/**
+		 * Constructor
+		 *
+		 * @since  1.0.0
+		 */
+		private function __construct() {
+			add_action( 'wp', array( $this, 'init' ) );
+		}
+
+		/**
+		 * Initialize the plugin only when the WP_Query is ready.
+		 *
+		 * @since  1.0.0
+		 */
+		public function init() {
+			$hidden_posts = get_option( 'hf_hide_post', array() );
+
+			// bail if none of the posts are hidden or we are on admin page or singular page.
+			if ( is_admin() || is_singular() || empty( $hidden_posts ) ) {
+				return '';
+			}
+
+			add_filter( 'posts_where', array( $this, 'where_clause' ), 20, 2 );
+			add_filter( 'get_next_post_where', array( $this, 'post_navigation_clause' ), 20, 1 );
+			add_filter( 'get_previous_post_where', array( $this, 'post_navigation_clause' ), 20, 1 );
+
+			$this->includes();
+		}
+
+		/**
+		 * Include required files.
+		 *
+		 * @since 1.0.0
+		 */
+		public function includes() {
+
+			if ( is_admin() ) {
+				require_once ODA_DIR . 'admin-functions.php';
+			}
+
+		}
+
+		/**
+		 * Filter where clause to hide selected posts.
+		 *
+		 * @since  1.0.0
+		 *
+		 * @param  String $where Where clause.
+		 * @param  WP_Query $query WP_Query &$this The WP_Query instance (passed by reference).
+		 *
+		 * @return String $where Where clause.
+		 */
+		function where_clause( $where, $query ) {
+			global $wpdb;
+			$where .= ' AND ' . $wpdb->prefix . 'posts.ID NOT IN ( ' . esc_sql( $this->hidden_post_string() ) . ' )';
+
+			return $where;
+		}
+
+		/**
+		 * Filter post navigation query to hide the selected posts.
+		 *
+		 * @since  1.0.0
+		 *
+		 * @param  String $where Where clause.
+		 *
+		 * @param  String $where Where clause.
+		 */
+		function post_navigation_clause( $where ) {
+			$where .= ' AND p.ID NOT IN ( ' . esc_sql( $this->hidden_post_string() ) . ' )';
+
+			return $where;
+		}
+
+		/**
+		 * Convert the array of posts to comma separated string to make it compatible to wpdb query.
+		 *
+		 * @since  1.0.0
+		 *
+		 * @return String Comma separated string of post id's.
+		 */
+		function hidden_post_string() {
+			$hidden_posts = get_option( 'hf_hide_post', array() );
+
+			return implode( ', ', $hidden_posts );
+		}
+
+	}
+
+	Hide_Posts::instance();
+}
