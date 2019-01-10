5a891f3574289689985656e2d89d9e04259d9cbb
Nikhil <email@nikhilchavan.com>
2019-01-10 11:31:13 +0530

    cs-fixes

--- class-unlist-posts.php
+++ class-unlist-posts.php
@@ -126,7 +126,7 @@ if ( ! class_exists( 'Unlist_Posts' ) ) {
 			$hidden_posts = get_option( 'unlist_posts', array() );
 
 			// bail if none of the posts are hidden or we are on admin page or singular page.
-			if ( ( is_admin() && ! wp_doing_ajax() )|| in_array( get_the_ID(), $hidden_posts ) || empty( $hidden_posts ) ) {
+			if ( ( is_admin() && ! wp_doing_ajax() ) || in_array( get_the_ID(), $hidden_posts ) || empty( $hidden_posts ) ) {
 				return $where;
 			}
 
