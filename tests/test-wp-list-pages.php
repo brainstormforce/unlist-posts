f2ccabcf4ce9f8d398b5f3544eda1218b79017e0
Vrunda <vrundak@bsf.io>
2021-07-08 18:27:43 +0530

    PHPCS

--- tests/test-wp-list-pages.php
+++ tests/test-wp-list-pages.php
@@ -34,71 +34,71 @@ class TestWPListPagesUnlisted extends WP_UnitTestCase {
 	 * Test to make sure unlisted posts are hidden from wp_list_pages_excludes filter.
 	 */
 	public function test_unlisted_post_not_in_wp_list_pages() {
-        _delete_all_posts();
+		_delete_all_posts();
 		wp_set_current_user( $this->editor_user_id );
 
-        // Listed Post.
-		$listed_post   = self::factory()->post->create(
+		// Listed Post.
+		$listed_post = self::factory()->post->create(
 			array(
 				'post_title' => 'Listed Post Title',
-                'post_type' => 'page'
+				'post_type' => 'page',
 			)
 		);
 
-        // Unlisted Post.
-        $unlisted_post = self::factory()->post->create(
+		// Unlisted Post.
+		$unlisted_post = self::factory()->post->create(
 			array(
 				'post_title' => 'Unlisted Post Title',
-                'post_type' => 'page'
+				'post_type' => 'page',
 			)
 		);
-        $_POST['unlist_posts']       = true;
+		$_POST['unlist_posts']       = true;
 		$_POST['unlist_post_nounce'] = wp_create_nonce( 'unlist_post_nounce' );
 		Unlist_Posts_Admin::instance()->save_meta( $unlisted_post );
 
 		$list_pages_markup = wp_list_pages(
-            array(
-                'echo' => false,
-            )
-        );
+			array(
+				'echo' => false,
+			)
+		);
 
 		// Assert that the Unlisted Post is visible in wp_list_page().
-        $this->assertNotContains( 'Unlisted Post Title', $list_pages_markup );
+		$this->assertNotContains( 'Unlisted Post Title', $list_pages_markup );
 	}
 
-    /**
+	/**
 	 * Test to make sure unlisted posts are hidden from wp_list_pages_excludes filter.
 	 */
 	public function test_listed_post_in_wp_list_pages() {
-        _delete_all_posts();
+		_delete_all_posts();
 		wp_set_current_user( $this->editor_user_id );
 
-        // Listed Post.
-		$listed_post   = self::factory()->post->create(
+		// Listed Post.
+		$listed_post = self::factory()->post->create(
 			array(
 				'post_title' => 'Listed Post Title',
-                'post_type' => 'page'
+				'post_type' => 'page',
 			)
 		);
 
-        // Unlisted Post.
-        $unlisted_post = self::factory()->post->create(
+		// Unlisted Post.
+		$unlisted_post = self::factory()->post->create(
 			array(
 				'post_title' => 'Unlisted Post Title',
-                'post_type' => 'page'
+				'post_type' => 'page',
 			)
 		);
-        $_POST['unlist_posts']       = true;
+		$_POST['unlist_posts']       = true;
 		$_POST['unlist_post_nounce'] = wp_create_nonce( 'unlist_post_nounce' );
 		Unlist_Posts_Admin::instance()->save_meta( $unlisted_post );
 
 		$list_pages_markup = wp_list_pages(
-            array(
-                'echo' => false,
-            )
-        );
+			array(
+				'echo' => false,
+			)
+		);
 
 		// Assert that the Listed Post is visible in wp_list_page().
-        $this->assertContains( 'Listed Post Title', $list_pages_markup );
+		$this->assertContains( 'Listed Post Title', $list_pages_markup );
 	}
 }
